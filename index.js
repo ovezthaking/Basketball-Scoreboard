8c1520955f7a2c5da49db79b47f075ef7b7825c4
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-16 02:38:48 +0100

    Popraw poprawność porównań w funkcjach dodawania punktów

--- index.js
+++ index.js
@@ -8,33 +8,33 @@ guestPoints.textContent = 0
 
 const add1 = () => {
     //console.log(event.target.parentElement.id)
-    if (event.target.parentElement.id == 'homes'){
+    if (event.target.parentElement.id === 'homes'){
         homePoints.innerText = parseInt(homePoints.textContent) + 1
     }
 
-    if (event.target.parentElement.id == 'guests'){
+    if (event.target.parentElement.id === 'guests'){
         guestPoints.innerText = parseInt(guestPoints.textContent) + 1
     }
 }
 
 
 const add2 = () => {
-    if (event.target.parentElement.id == 'homes'){
+    if (event.target.parentElement.id === 'homes'){
         homePoints.innerText = parseInt(homePoints.textContent) + 2
     }
 
-    if (event.target.parentElement.id == 'guests'){
+    if (event.target.parentElement.id === 'guests'){
         guestPoints.innerText = parseInt(guestPoints.textContent) + 2
     }
 }
 
 
 const add3 = () => {
-    if (event.target.parentElement.id == 'homes'){
+    if (event.target.parentElement.id === 'homes'){
         homePoints.innerText = parseInt(homePoints.textContent) + 3
     }
 
-    if (event.target.parentElement.id == 'guests'){
+    if (event.target.parentElement.id === 'guests'){
         guestPoints.innerText = parseInt(guestPoints.textContent) + 3
     }
 }
